89d6b0e588220bc06d268ce67ae19ba9d573f367
Luca Piccinelli <Luca Piccinelli>
2016-03-31 08:00:57 +0200

    estrazione delle aree dei rettangolo e disegno sull'immagine finale

--- barcodeTutorial.py
+++ barcodeTutorial.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 im = cv2.imread(r'img\barcodes.jpg', cv2.IMREAD_GRAYSCALE)
+im_out = cv2.imread(r'img\barcodes.jpg')
 
 #riscalatura dell'immagine
 scale = 800.0 / im.shape[1]
@@ -23,5 +24,29 @@ im = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel, anchor=(2, 0), iterations=2) 
 kernel = np.ones((21, 35), np.uint8)
 im = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel, iterations=1)
 
-plt.imshow(im, cmap='Greys_r')
+#estrazione dei componenti connessi
+contours, hierarchy = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+unscale = 1.0 / scale
+if contours != None:
+    for contour in contours:
+        
+        # se l'area non è grande a sufficienza la salto 
+        if cv2.contourArea(contour) <= 2000:
+            continue
+        
+        #estraggo il rettangolo di area minima (in formato (centro_x, centro_y), (width, height), angolo)
+        rect = cv2.minAreaRect(contour)
+        #l'effetto della riscalatura iniziale deve essere eliminato dalle coordinate rilevate
+        rect = \
+            ((int(rect[0][0] * unscale), int(rect[0][1] * unscale)), \
+             (int(rect[1][0] * unscale), int(rect[1][1] * unscale)), \
+             rect[2])
+        
+        
+        #disegno il tutto sull'immagine originale
+        box = np.int0(cv2.cv.BoxPoints(rect))
+        cv2.drawContours(im_out, [box], 0, (0, 255, 0), thickness = 2)
+        
+plt.imshow(im_out)
+cv2.imwrite(r'img\out.png', im_out)
