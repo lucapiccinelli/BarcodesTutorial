d672141c36b8a41f16d98838b51d6668fcaf815d
Luca Piccinelli <Luca Piccinelli>
2016-03-31 07:40:52 +0200

    operazioni morfologiche per ottenere l'immagine dei componenti connessi

--- barcodeTutorial.py
+++ barcodeTutorial.py
@@ -4,12 +4,24 @@ import numpy as np
 
 im = cv2.imread(r'img\barcodes.jpg', cv2.IMREAD_GRAYSCALE)
 
+#riscalatura dell'immagine
 scale = 800.0 / im.shape[1]
 im = cv2.resize(im, (int(im.shape[1] * scale), int(im.shape[0] * scale)))
 
+#blackhat
 kernel = np.ones((1, 3), np.uint8)
 im = cv2.morphologyEx(im, cv2.MORPH_BLACKHAT, kernel, anchor=(1, 0))
 
+#sogliatura
 thresh, im = cv2.threshold(im, 10, 255, cv2.THRESH_BINARY)
 
+#operazioni  morfologiche
+kernel = np.ones((1, 5), np.uint8)
+im = cv2.morphologyEx(im, cv2.MORPH_DILATE, kernel, anchor=(2, 0), iterations=2) #dilatazione
+im = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel, anchor=(2, 0), iterations=2)  #chiusura
+
+kernel = np.ones((21, 35), np.uint8)
+im = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel, iterations=1)
+
 plt.imshow(im, cmap='Greys_r')
+
